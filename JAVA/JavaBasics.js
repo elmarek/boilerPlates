947342c8c7c2e75a0fab1d809675e7fb3190cd32
elmarek <mareks@gmx.com>
2021-03-02 15:05:55 -0700

    Java + Basics

---
+++ JAVA/JavaBasics.js
@@ -0,0 +1,16 @@
+
+
+2.  import java.io.*: Means all the classes of io package can be imported. Java io package provides a set of input and output streams for reading and writing data to files or other input or output sources
+
+3.  'class': The class contains the data and methods to be used in the program. Methods define the behaviour of the class. Class GFG has only one method Main in JAVA.
+
+4.  'static void Main()': static keyword tells us that this method is accessible without instantiating the class.
+
+5.  'void': keywords tells that this method will not return anything. The main() method is the entry point of our application.
+
+6. ' System.in': This is the standard input stream that is used to read characters from the keyboard or any other standard input device.
+
+7.  'System.out': This is the standard output stream that is used to produce the result of a program on an output device like the computer screen.
+
+8.  println(): This method in Java is also used to display a text on the console. It prints the text on the console and the cursor moves to the start of the next line at the console. The next printing takes place from the next line.
+
