7fa95e56ca01e9fe7af83aa9cfedd326bd8ca0b2
elmarek <mareks@gmx.com>
2021-03-16 17:06:23 -0700

    Java identifiers

--- JAVA/JavaBasics.js
+++ JAVA/JavaBasics.js
@@ -54,8 +54,51 @@ public static void main(String[] args)
 public: So that JVM can execute the method from anywhere.
 static: Main method is to be called without object.
 The modifiers public and static can be written in either order.
-void: The main method doesn't return anything.
-main(): Name configured in the JVM.
-String[]: The main method accepts a single argument:
-          an array of elements of type String.
-Like in C/C++, main method is the entry point for your application and will subsequently invoke all the other methods required by your program.
+void: The main method doesn't return anything....
+main(){}: Name configured in the JVM
+`String[]: The main method accepts a single argument:
+          an array of elements of type String.`
+// Like in C/C++, main method is the entry point for your application and will subsequently invoke all the other methods required by your program.
+
+
+`Java Identifiers`
+All Java components require names. Names used for classes, variables, and methods are called identifiers.
+
+In Java, there are several points to remember about identifiers. They are as follows −
+
+All identifiers should begin with a letter (A to Z or a to z), currency character ($) or an underscore (_).
+
+After the first character, identifiers can have any combination of characters.
+
+A key word cannot be used as an identifier.
+
+Most importantly, identifiers are case sensitive.
+
+Examples of legal identifiers: age, $salary, _value, __1_value.
+
+Examples of illegal identifiers: 123abc, -salary.
+
+'Java Modifiers'
+Like other languages, it is possible to modify classes, methods, etc., by using modifiers. There are two categories of modifiers −
+
+Access Modifiers − default, public , protected, private
+
+Non-access Modifiers − final, abstract, strictfp
+
+We will be looking into more details about modifiers in the next section.
+
+'Java Variables'
+Following are the types of variables in Java −
+
+'Local Variables'
+Class Variables (Static Variables)
+Instance Variables (Non-static Variables)
+Java Arrays
+Arrays are objects that store multiple variables of the same type. However, an array itself is an object on the heap. We will look into how to declare, construct, and initialize in the upcoming chapters.
+
+'Java Enums'
+Enums were introduced in Java 5.0. Enums restrict a variable to have one of only a few predefined values. The values in this enumerated list are called enums.
+
+With the use of enums it is possible to reduce the number of bugs in your code.
+
+For example, if we consider an application for a fresh juice shop, it would be possible to restrict the glass size to small, medium, and large. This would make sure that it would not allow anyone to order any size other than small, medium, or large.
